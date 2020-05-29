89df1e1b1f455d9d4434885ecf38f94002a33f48
Abigail Nielson <aa.nielson18@gmail.com>
2020-05-29 16:00:05 -0700

    added instructions to the readme and input them into the necessary files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- Still in the db folder, create a seeds.sql file.
+-- In this file, write insert queries to populate the burgers table with at least three entries.
+
+INSERT INTO burgers (burger_name) VALUES ('BigMac');
+INSERT INTO burgers (burger_name) VALUES ('South of the Border Burger');
+INSERT INTO burgers (burger_name) VALUES ('Baconator');
+
+SELECT * FROM burgers;
