4b610686e8f82baa7d81ed5b6137eaeeef047420
Abigail Nielson <aa.nielson18@gmail.com>
2020-06-30 17:58:18 -0600

    added functionality for updating the devoured boolean and re-rendering the page. still needs work

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,54 +9,68 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create the router for the app, and export the router at the end of your file.
-router.get("/", function(req, res) {
-    burger.select(function(data) {
-      var hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
+router.get("/", function (req, res) {
+  burger.select(function (data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
-router.post("/api/burgers", function(req, res) {
-    burger.create([
-      "burger_name", "devoured"
-    ], [
-      req.body.burger_name, req.body.devoured
-    ], function(result) {
-      // Send back the ID of the new quote
-      //redirect to /
-      res.redirect("/");
-    });
+router.post("/api/burgers", function (req, res) {
+  burger.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function (result) {
+    // Send back the ID of the new quote
+    //redirect to /
+    res.redirect("/");
+  });
 });
-router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-  
-    console.log("condition: ", condition);
-  
+router.put("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+  console.log(req.body.devoured);
+  console.log("condition: ", condition);
+  if (!req.body.devoured) {
     burger.update({
-        devoured: req.body.devoured
-    }, condition, function(result) {
+      devoured: true
+    }, condition, function (result) {
       if (result.changedRows == 0) {
+        console.log("If is happening");
         // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
+        res.status(200);
       } else {
-        res.status(200).end();
+        res.status(200);
       }
     });
+  } else {
+    burger.update({
+      devoured: false
+    }, condition, function (result) {
+      if (result.changedRows == 0) {
+        console.log("If is happening");
+        // If no rows were changed, then the ID must not exist, so 404
+        res.status(200);
+      } else {
+        res.status(200);
+      }
+    });
+  }
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-  
-    burger.delete(condition, function(result) {
-      if (result.affectedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
+router.delete("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function (result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
 });
 
 // Export routes for server.js to use.
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,16 +5,28 @@ $(document).ready(function() {
     console.log("The button click worked");
     
     var burgerId = $(this).attr('data-id');
+    var newDevoured = $(this).attr('data-newdevoured');
+    if (!newDevoured) {
+      $.ajax({
+        url: "/api/burgers/" + burgerId,
+        method: "PUT",
+        data: {devoured: false}
+      }).then(function(){
+        console.log("Then is working");
+        // location.reload();
+      })
+    } else {
+      $.ajax({
+        url: "/api/burgers/" + burgerId,
+        method: "PUT",
+        data: {devoured: true}
+      }).then(function(){
+        console.log("Then is working");
+        // location.reload();
+      })
+    }
+    console.log(burgerId);
     //update .newdevoured to true if false and false if true
-    if ($(this).attr('newdevoured') === false) {
-      $(this).attr('newdevoured') = true
-    }
-    if ($(this).attr('newdevoured') === true) {
-      $(this).attr('newdevoured') = false;
-    }
-
-  });
-
-   
+  }); 
 });
   
